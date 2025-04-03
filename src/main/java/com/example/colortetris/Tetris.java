090394a55e154362c647e1ff4c878c236de5b56c
KleSai19 <Klesai19@htl-shkoder.com>
2025-04-03 09:03:56 +0200

    commit

--- src/main/java/com/example/colortetris/Tetris.java
+++ src/main/java/com/example/colortetris/Tetris.java
@@ -1,26 +1,14 @@
 package com.example.colortetris;
-import java.util.Acraxlist;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Timer;
 import java.util.TimerTask;
-
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event .EventHandler;
-unport
-import
-        javafxascene.Nodes
-javafx.scene.Scene:
-import
-        javafa scene.input .KeyEvent;
-import
-        invafa.scene layout Pane:
-import javafx.scene.paint.Color:
-import
-        javef-scene shape.Line:
-import
-        javafx.scene, shape. Rectangle;
-import javafx.scene.text.Text;
-inport jaxatz,stage,Stage:
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+
 public class Tetris {
 }
